b223914104501d788c8b355a5c4d2bcb7f6fefd0
AbuSaalim <salim.whitestone@gmail.com>
2025-05-06 18:44:17 +0530

    today work on X printing or printing

---
+++ starPrinting.js
@@ -0,0 +1,144 @@
+const prompt = require("prompt-sync")();
+
+// process.stdout.write("Hello ");
+// process.stdout.write("World");
+
+// let n = prompt("Enter A Number : ");
+// // let a = 5;
+// for (let i = 0; i <= n; i++) {
+//   //   console.log("*");
+//   for (let j = 1; j <= n; j++) {
+//     process.stdout.write(" * ");
+//   }
+//   console.log();
+// }
+
+//////    1. Star Printing     ///////
+/*
+ *
+ *  *
+ *  *  *
+ *  *  *  *
+ *  *  *  *  *
+ */
+
+// let n = Number(prompt("Enter Number"));
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+//////    2. Number Printing     ///////
+
+// let n = Number(prompt("Number : "));
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write(j + " ");
+//   }
+//   console.log();
+// }
+
+//////    3. ascii Number Printing     ///////
+
+// let n = Number(prompt("Number : "));
+// for (let i = 1; i <= n; i++) {
+//   let ascii = 65;
+//   for (let j = 1; j <= i; j++) {
+//     process.stdout.write(String.fromCharCode(ascii) + " ");
+//     ascii++;
+//   }
+//   console.log();
+// }
+
+//////    4. Star Printing (reverse)     ///////
+/*
+ *  *  *  *  *
+ *  *  *  *
+ *  *  *
+ *  *
+ *
+ */
+
+// let n = Number(prompt("Enter Number"));
+
+// for (let i = 1; i <= n; i++) {
+//   /////   1  2   3  4  5
+//   for (let j = i; j <= n; j++) {
+//     /////   5  4   3  2  1
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+//////    4. Star Printing (reverse) alernate solution     ///////
+
+// let n = Number(prompt("Enter Number"));
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n - i + 1; j++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+//////    4. Star Printing (reverse) alernate solution     ///////
+
+// let n = Number(prompt("Enter Number: "));
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n - i + 1; j++) {
+//     process.stdout.write("_ ");
+//   }
+//   for (let k = 1; k <= i; k++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+//////    5. Star Printing (reverse) alernate solution     ///////
+
+// let n = Number(prompt("Enter Number: "));
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n - i + 1; j++) {
+//     process.stdout.write(" ");
+//   }
+//   for (let k = 1; k <= i; k++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+//////    5. Star Printing X      ///////
+
+// let n = Number(prompt("Enter A Number : "));
+// for (let i = 1; i <= n; i++) {
+//   // process.stdout.write(`${i} or ${n}`);
+//   //   console.log(`${i}, ${n}`);
+//   for (let j = 1; j <= n; j++) {
+//     if (i === j || i + j == n + 1) {
+//       process.stdout.write("*");
+//     } else {
+//       process.stdout.write(" ");
+//     }
+//   }
+//   console.log();
+// }
+
+//////    6. Star Printing V      ///////
+
+let n = Number(prompt("Enter A Number : "));
+
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= 2 * n - 1; j++) {
+    if (i === j || i + j == 2 * n) {
+      process.stdout.write("*");
+    } else {
+      process.stdout.write(" ");
+    }
+  }
+  console.log();
+}
